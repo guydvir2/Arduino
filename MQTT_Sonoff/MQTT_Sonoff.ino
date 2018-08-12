4b2f9919786c5851d73636a8890ca7adb6fc1ab7
guyd@linux <guydvir.tech@gmail.com>
2018-08-12 21:56:45 +0300

    from ubu

--- MQTT_Sonoff/MQTT_Sonoff.ino
+++ MQTT_Sonoff/MQTT_Sonoff.ino
@@ -17,11 +17,13 @@ PubSubClient client(espClient);
 
 
 char msg[50];
+const char* clientID = "Sonoff";
+char *client_temp = "HomePi/Dvir/Windows/";
 const char* outTopic = "HomePi/Dvir/Messages";
 const char* outTopic2 = "HomePi/Dvir/Windows/ESP32";
-const char* inTopic = "HomePi/Dvir/Windows/SONOFF";
+const char* inTopic [strlen(client_temp) + 3];
 const char* inTopic2 = "HomePi/Dvir/Windows/All";
-const char* clientID = "Sonoff#1";
+
 String clock;
 
 void start_wifi() {
