85b6dbff58d362c3faa4c663caae105aa149cc6d
GUYDVIR <guydvir2@gmail.com>
2018-12-04 13:03:32 +0200

    grom work

--- SmartRel_esp_standalone/SmartRel_esp_standalone.ino
+++ SmartRel_esp_standalone/SmartRel_esp_standalone.ino
@@ -11,7 +11,7 @@ bool useOTA = true;
 bool runPbit = false;
 int networkID = 1;  // 0: HomeNetwork,  1:Xiaomi_D6C8
 
-#include <Arduino.h>
+// #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <TimeLib.h>
 #include <NtpClientLib.h>
@@ -79,7 +79,7 @@ long noNetwork_Counter=0;
 int manResetCounter = 0;  // reset press counter
 int pressAmount2Reset = 3; // time to press button to init Reset
 long lastResetPress = 0; // time stamp of last press
-long resetTimer = 0;
+// long resetTimer = 0;
 // ####################
 
 
@@ -118,7 +118,7 @@ bool inputDown_currentState;
 #define SwitchOn LOW
 
 // #####################################################
-
+// ~~~~~ setup & definitions ~~~~
 void setup() {
         startGPIOs();// <------------ Need to be changed
 
@@ -126,10 +126,8 @@ void setup() {
                 Serial.begin(9600);
                 delay(10);
                 Serial.println("SystemBoot");
-
         }
         if (useNetwork) {
-                selectNetwork();
                 startNetwork();
         }
         if(useOTA) {
@@ -142,7 +140,6 @@ void setup() {
                 PBit(); // PowerOn Bit
         }
 }
-
 void startGPIOs() {
         // <------------ Need to be changed
         pinMode(inputUpPin, INPUT_PULLUP);
@@ -154,26 +151,17 @@ void startGPIOs() {
 }
 
 // Common ##############
-void selectNetwork() {
-        if (networkID == 1 ) {
-                ssid = ssid_1;
-                mqtt_server = mqtt_server_1;
-        }
-        else {
-                ssid = ssid_0;
-                mqtt_server = mqtt_server_0;
-        }
-}
-
+// ~~~~~~~ Network connectivity ~~~~~
 void startNetwork() {
         long startWifiConnection = millis();
+
+        selectNetwork();
         if (useSerial) {
                 Serial.println();
                 Serial.print("Connecting to ");
                 Serial.println(ssid);
         }
 
-        // startWifiConnection = millis();
         WiFi.mode(WIFI_STA); //OTA Added
         WiFi.begin(ssid, password);
 
@@ -188,10 +176,8 @@ void startNetwork() {
         // case of no success - restart due to no wifi
         if (WiFi.status() != WL_CONNECTED) {
                 if (useSerial) {
-                        Serial.println("reset- no wifi detected");
+                        Serial.println("no wifi detected");
                 }
-                // sendReset("null");
-
         }
 
         // if wifi is OK
@@ -203,6 +189,7 @@ void startNetwork() {
                         Serial.print("IP address: ");
                         Serial.println(WiFi.localIP());
                 }
+
                 startMQTT();
                 startNTP();
                 get_timeStamp();
@@ -210,7 +197,47 @@ void startNetwork() {
                 connectMQTT();
         }
 }
+int networkStatus(){
+        if (WiFi.status() == WL_CONNECTED && mqttClient.connected()) {
+                mqttClient.loop();
+                // noNetwork_Counter = 0;
+                return 1;
+        }
+        else {
+                if (noNetwork_Counter == 0) {
+                        noNetwork_Counter = millis();
+                        useNetwork = false;
+                }
+                return 0;
+        }
+}
+void network_check(){
+        if ( networkStatus() == 0) {
+                if (millis()-noNetwork_Counter >= time2Reset) {
+                        sendReset("null");
+                }
+                if (millis()-noNetwork_Counter >= time2tryConnect) {
+                        startNetwork();
+                        noNetwork_Counter = 0;
+                }
+        }
+}
+void selectNetwork() {
+        if (networkID == 1 ) {
+                ssid = ssid_1;
+                mqtt_server = mqtt_server_1;
+        }
+        else {
+                ssid = ssid_0;
+                mqtt_server = mqtt_server_0;
+        }
+}
 
+// ~~~~ Services ~~~~~~
+void startNTP() {
+        NTP.begin("pool.ntp.org", 2, true);
+        NTP.setInterval(1000 * 3600 * clockUpdateInt);
+}
 void startOTA() {
         char OTAname[100];
         int m = 0;
@@ -280,17 +307,12 @@ void startOTA() {
         ArduinoOTA.begin();
 }
 
+// ~~~~~~~ MQTT functions ~~~~~~~
 void startMQTT() {
         createTopics(deviceTopic, stateTopic, availTopic);
         mqttClient.setServer(mqtt_server, 1883);
         mqttClient.setCallback(callback);
 }
-
-void startNTP() {
-        NTP.begin("pool.ntp.org", 2, true);
-        NTP.setInterval(1000 * 3600 * clockUpdateInt);
-}
-
 int connectMQTT() {
         long startClock = millis();
 
@@ -350,91 +372,10 @@ int connectMQTT() {
                 return 0;
         }
 }
-
-void verifyMQTTConnection() {
-        if (WiFi.status() == WL_CONNECTED) {
-                //  MQTT reconnection for first time or after first insuccess to reconnect
-                if (mqttClient.connected()) {
-                        mqttClient.loop();
-                }
-
-
-                else if (!mqttClient.connected() && firstNotConnected == 0) {
-                        connectionFlag = connectMQTT(); // few tries
-                        //  still not connected
-                        if (connectionFlag == 0 ) {
-                                firstNotConnected = millis();
-                        }
-                        else {
-                                mqttClient.loop();
-                        }
-                }
-                // retry after fail - resume only after timeout
-                else if (!mqttClient.connected() && firstNotConnected != 0 && millis() - firstNotConnected > MQTTtimeOut) {
-                        //after cooling out period - try agai
-                        if (useSerial) {
-                                Serial.println("trying again to reconnect");
-                        }
-                        connectionFlag = connectMQTT();
-                        // still no connection - send a reset signal
-                        if (connectionFlag==0) {
-                                sendReset("null");
-                                if (useSerial) {
-                                        Serial.println("reset after no MQTT");
-                                }
-                        }
-                        // reconnection succeeded after timeout
-                        else {
-                                firstNotConnected = 0;
-                        }
-                }
-
-                else if (mqttClient.connected()) {
-                        mqttClient.loop();
-                }
-        }
-
-        // retry connect wifi
-        else {
-                if (useSerial) {
-                        Serial.println("try to reconnect wifi");
-                }
-                startNetwork();
-        }
-}
-
-int networkStatus(){
-        if (WiFi.status() == WL_CONNECTED &&mqttClient.connected()) {
-                mqttClient.loop();
-                noNetwork_Counter = 0;
-                return 1;
-        }
-        else {
-                if (noNetwork_Counter == 0) {
-                        noNetwork_Counter = millis();
-                }
-                return 0;
-        }
-}
-
-void network_check(){
-        if ( networkStatus() == 0) {
-                if (millis()-noNetwork_Counter >= time2Reset) {
-                        sendReset("null");
-                }
-                if (millis()-noNetwork_Counter >= time2tryConnect) {
-                        startNetwork();
-                        noNetwork_Counter = 0;
-                }
-        }
-
-}
-
 void createTopics(const char *devTopic, char *state, char *avail) {
         sprintf(state, "%s/State", devTopic);
         sprintf(avail, "%s/Avail", devTopic);
 }
-
 void callback(char* topic, byte* payload, unsigned int length) {
         char incoming_msg[50];
         char state[5];
@@ -521,7 +462,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
                 sendReset("MQTT");
         }
 }
-
 void pub_msg(char *inmsg) {
         char tmpmsg[150];
 
@@ -531,7 +471,6 @@ void pub_msg(char *inmsg) {
                 msgSplitter(inmsg, 95, tmpmsg, "#" );
         }
 }
-
 void msgSplitter( const char* msg_in, int max_msgSize, char *prefix, char *split_msg) {
         char tmp[120];
 
@@ -557,46 +496,62 @@ void msgSplitter( const char* msg_in, int max_msgSize, char *prefix, char *split
                         mqttClient.publish(msgTopic, tmp);
                 }}
 }
-
 void get_timeStamp() {
         time_t t = now();
         sprintf(timeStamp, "%02d-%02d-%02d %02d:%02d:%02d", year(t), month(t), day(t), hour(t), minute(t), second(t));
 }
 
+// ~~~~~~ Reset and maintability ~~~~~~
 void sendReset(char *header) {
         char temp[150];
 
         if (useSerial) {
                 Serial.println("Sending Reset command");
         }
-        if (useNetwork) {
                 if (strcmp(header, "null")!=0) {
                         sprintf(temp, "[%s] - Reset sent", header);
                         pub_msg(temp);
                 }
-        }
-        // delay(100);
         ESP.reset();
 }
-
 void feedTheDog() {
         wdtResetCounter++;
         if (wdtResetCounter >= wdtMaxRetries) {
                 sendReset("WatchDog");
         }
 }
-
 void acceptOTA() {
         if (millis() - OTAcounter <= OTAtimeOut) {
                 ArduinoOTA.handle();
         }
 }
-
 // END Common ############
 
 
 
 // Code specific #######################
+// ~~~~ maintability ~~~~~~
+void PBit() {
+        int pause = 2 * 5 * deBounceInt;
+        allOff();
+        delay(pause);
+        digitalWrite(outputUpPin, RelayOn);
+        delay(pause);
+        digitalWrite(outputUpPin, !RelayOn);
+        delay(pause);
+        digitalWrite(outputDownPin, RelayOn);
+        delay(pause);
+        allOff();
+
+}
+void allOff() {
+        digitalWrite(outputUpPin, !RelayOn);
+        digitalWrite(outputDownPin, !RelayOn);
+        inputUp_lastState = digitalRead(inputUpPin);
+        inputDown_lastState = digitalRead(inputDownPin);
+}
+
+// ~~~~~~~~~ GPIO switching ~~~~~~~~~~~~~
 void switchIt(char *type, char *dir) {
         char mqttmsg[50];
         bool states[2];
@@ -636,7 +591,6 @@ void switchIt(char *type, char *dir) {
                 Serial.println(dir);
         }
 }
-
 void detectResetPresses() {
         if (millis() - lastResetPress < timeInt2Reset) {
                 if (useSerial) {
@@ -658,28 +612,6 @@ void detectResetPresses() {
                 manResetCounter = 0;
         }
 }
-
-void PBit() {
-        int pause = 2 * 5 * deBounceInt;
-        allOff();
-        delay(pause);
-        digitalWrite(outputUpPin, RelayOn);
-        delay(pause);
-        digitalWrite(outputUpPin, !RelayOn);
-        delay(pause);
-        digitalWrite(outputDownPin, RelayOn);
-        delay(pause);
-        allOff();
-
-}
-
-void allOff() {
-        digitalWrite(outputUpPin, !RelayOn);
-        digitalWrite(outputDownPin, !RelayOn);
-        inputUp_lastState = digitalRead(inputUpPin);
-        inputDown_lastState = digitalRead(inputDownPin);
-}
-
 void checkSwitch_PressedUp() {
         bool temp_inputUpPin = digitalRead(inputUpPin);
 
@@ -702,14 +634,11 @@ void checkSwitch_PressedUp() {
                 else { // for debug only
                         char tMsg [100];
                         sprintf(tMsg, "UP Bounce: cRead [%d] lRead[%d]", temp_inputUpPin, inputUp_lastState);
-                        if (useNetwork) {
                                 pub_msg(tMsg);
-                        }
                 }
         }
 
 }
-
 void checkSwitch_PressedDown() {
         bool temp_inputDownPin = digitalRead(inputDownPin);
 
@@ -727,15 +656,12 @@ void checkSwitch_PressedDown() {
                         }
                 }
                 else { // for debug only
-                        if (useNetwork) {
                                 char tMsg [100];
                                 sprintf(tMsg, "Down Bounce: cRead[%d] lRead[%d]", temp_inputDownPin, inputDown_lastState);
                                 pub_msg(tMsg);
-                        }
                 }
         }
 }
-
 void verifyNotHazardState() {
         if (outputUp_currentState == RelayOn && outputDown_currentState == RelayOn ) {
                 switchIt("Button", "off");
@@ -746,14 +672,12 @@ void verifyNotHazardState() {
         }
 
 }
-
 void readGpioStates() {
         outputUp_currentState = digitalRead(outputUpPin);
         outputDown_currentState = digitalRead(outputDownPin);
         inputDown_currentState = digitalRead(inputDownPin);
         inputUp_currentState = digitalRead(inputUpPin);
 }
-
 void loop() {
         // read GPIOs
         readGpioStates();
@@ -762,7 +686,6 @@ void loop() {
         // Service updates
         if (useNetwork) {
           network_check();
-                // verifyMQTTConnection();
         }
         if (useWDT) {
                 wdtResetCounter = 0;
