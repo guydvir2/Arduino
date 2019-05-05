2b138266bbda505dea8e2c6ee3f60c92a37f67ff
Guy Dvir <guydvir2@gmail.com>
2019-05-05 23:50:36 +0300

    update eds

--- HomePi/ClosetLed2/ClosetLed2.ino
+++ HomePi/ClosetLed2/ClosetLed2.ino
@@ -1,117 +1,162 @@
-//#include <avr/sleep.h>
+#include <avr/sleep.h>
 
+#define NUM_SENSORS     1        // <----- NEED TO CHANGE BY USER
+#define PWRDOWN_TIMEOUT 1000 * 5 // <----- NEED TO CHANGE BY USER
 #define RelayON HIGH
-#define lightON HIGH
 
-const int pwrdown_timeOut  = 1000 * 10;
-#define sensorPin_1 2
-//const int sensorPin_2      = 3;
+
+const int sensorPin_1      = 2;
+const int sensorPin_2      = 3;
 const int relayPin_1       = 4;
 const int relayPin_2       = 5;
-long onCounter             = 0;
-bool volatile doorOpen     = 0;
+long unsigned last_input   = 0;
+bool volatile doorOpen_1   = 0;
+bool volatile doorOpen_2   = 0;
+
+const int sensorsPin[2]             = {sensorPin_1, sensorPin_2};
+const int relaysPin[2]              = {relayPin_1, relayPin_2};
+bool volatile sensorsState[2]       = {false, false};
+bool volatile last_sensorsState[2]  = {false, false};
+long unsigned onCounters[2]         = {0, 0};
+long unsigned lastInputs[2]         = {0, 0};
+bool inTimeOUT [2]                  = {false, false};
 
 
-//void go2sleep() {
-//  sleep_enable();
-//  attachInterrupt(digitalPinToInterrupt(sensorPin_1), detection_door , LOW);
-//  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-//  Serial.println("Going2Sleep");
-//  delay(1000);
-////  sleep_cpu();
-//
+// ~~~~~~ Sleep~~~~~~~~~
+void go2sleep(int *sensAmount) {
+        Serial.print("go2sleep\n");
 
-//}
+        sleep_enable();
+        for (int i=0; i<sensAmount; i++) {
+                reAttach(i);
+        }
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+        sleep_cpu();
 
-void turnOff_timeout() {
-  if (pwrdown_timeOut != 0 && onCounter != 0) {
-    if (millis() - onCounter >= pwrdown_timeOut) {
-      Serial.println("Sleep!");
-      digitalWrite(relayPin_1, !lightON);
-    }
-  }
-  //  else if (  {
-  //    sleep_enable();
-  //    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  //    sleep_cpu();
-
-  //  }
-  //  if (pwrdown_timeOut == 0 || onCounter == 0) {
-  //    sleep_enable();
-  //    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  //    sleep_cpu();
-  //  }
-  //  else if ( millis() - onCounter >= pwrdown_timeOut) {
-  //    sleep_enable();
-  //    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  //    sleep_cpu();
-  //    // Serial.println("Sleep!");
-  //  }
-
+        // ~~~~~~~~~~~~~~~~~~~
+        Serial.print("wake\n");
+        // Serial.println(i);
+        delay(1000);
 }
+// ~~~~~~~~~~~~~~~~~~~~~
 
-void switchLeds() {
-  if (doorOpen == true) {
-    digitalWrite(relayPin_1, RelayON);
-    onCounter = millis();
-    attachInterrupt(digitalPinToInterrupt(sensorPin_1), detection_door , CHANGE);
+// ~~~ Switching Power ~~~~~~~
+void turnOff_relay(int i){
+        if (digitalRead(relaysPin[i]) == RelayON) {
+                digitalWrite(relaysPin[i], !RelayON);
+                last_sensorsState[i]=!RelayON;
+                sensorsState[i] = !RelayON;  // need for TO
+                onCounters[i] = 0;
 
-  }
-  else {
-    digitalWrite(relayPin_1, !RelayON);
-    onCounter = 0;
-    attachInterrupt(digitalPinToInterrupt(sensorPin_1), detection_door , CHANGE);
-
-  }
+                // Serial.print("Off, Sensor #");
+                // Serial.println(i);
+        }
 }
+void turnOn_relay(int i){
+        if (digitalRead(relaysPin[i])==!RelayON) {
+                digitalWrite(relaysPin[i], RelayON);
+                last_sensorsState[i] = RelayON;
+                sensorsState[i] = RelayON;  // need for TO
+                onCounters[i] = millis();
 
-void detection_door() {
-  Serial.println("Interrrupt Fired");//Print message to serial monitor
-  //  sleep_disable();
-  detachInterrupt(digitalPinToInterrupt(sensorPin_1));
-  doorOpen = digitalRead(sensorPin_1);
-
-  // if (digitalRead(_sensorPin) == doorOpen) {
-  //         _doorOpen = true;
-  //         Serial.println("OPEN");
-  // }
-  // else {// trun off
-  //         _doorOpen = false;
-  //         Serial.println("CLOSE");
-  // }
+                // Serial.print("On, Sensor #");
+                // Serial.println(i);
+        }
 }
+void checkSensor(int i) {
+        if (sensorsState[i]!=last_sensorsState[i]) { // enter on change only
+                // if (millis()-lastInputs[i]>100) { // ms of debounce
+                if (sensorsState[i] == true) {
+                        turnOn_relay(i);
+                }
+                else {
+                        turnOff_relay(i);
+                }
+                lastInputs[i] = millis();
+        }
+        // }
+}
+void offBy_timeout(int i){
+        if (PWRDOWN_TIMEOUT == 0 ) { // user not using TO
+                inTimeOUT[i] = false;
+        }
+        if (PWRDOWN_TIMEOUT > 0) { // user setup TO ?
+                if (millis()-onCounters[i]>=PWRDOWN_TIMEOUT) { //TO ended
+                        turnOff_relay(i);
+                        inTimeOUT[i] = false;
+                }
+                else { //Still in TO
+                        inTimeOUT[i] = true;
+                }
+        }
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~ Interrupts ~~~~~~~~
+void reAttach(int i){
+        if (i==0) {
+                attachInterrupt(digitalPinToInterrupt(sensorsPin[i]), sensor0_ISR, CHANGE);
+        }
+        if (i==1) {
+                attachInterrupt(digitalPinToInterrupt(sensorsPin[i]), sensor1_ISR, CHANGE);
+        }
+}
+void sensorActivated(int i) {
+        sleep_disable();
+        // Serial.print("Door, Sensor #");
+        // Serial.println(i);
+        sensorsState[i] = digitalRead(sensorsPin[i]);
+        detachInterrupt(digitalPinToInterrupt(sensorsPin[i]));
+}
+void sensor0_ISR(){
+        sensorActivated(0);
+}
+void sensor1_ISR(){
+        sensorActivated(1);
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~ Constructors ~~~~~~~
+void startSensors(int i){
+        pinMode(relaysPin[i], OUTPUT);
+        pinMode(sensorsPin[i], INPUT_PULLUP);
+        digitalWrite(relaysPin[i], RelayON);
+        last_sensorsState[i] = digitalRead(sensorsPin[i]);
+}
+void looperSensors(int i){
+        checkSensor(i);
+        offBy_timeout(i);
+        reAttach(i);
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Start");
-
-
-  pinMode(relayPin_1, OUTPUT);
-  pinMode(sensorPin_1, INPUT_PULLUP);
-  digitalWrite(relayPin_1, !lightON);
-
-  //  attachInterrupt(digitalPinToInterrupt(sensorPin_1), detection_door , CHANGE);
+        // Serial.begin(115200);
+        // startSensors(1);
+        startSensors(0);
 }
 
 void loop() {
-  //  go2sleep();
-  switchLeds();
-  turnOff_timeout();
+        delay(1000);
+
+        // looperSensors(1);
+        looperSensors(0);
+        int sum = 0;
+        for (int i=0; i<NUM_SENSORS; i++) {
+                sum += inTimeOUT[i];
+        }
+        // if (sum == 0){ //inTimeOUT[0]==false && inTimeOUT[1]==false) {
+        //   // Serial.println(sum);
+        //         go2sleep(NUM_SENSORS);
+        // }
+        // delay(100);
+        // if (inTimeOUT[0]==false && inTimeOUT[1]==false) {
+        //   // Serial.println(sum);
+        //         go2sleep(NUM_SENSORS);
+        // }
+        if (inTimeOUT[0]==false) {
+                // Serial.println(sum);
+                go2sleep(NUM_SENSORS);
+        }
+
 }
-
-//void Going_To_Sleep() {
-//  sleep_enable();//Enabling sleep mode
-//  attachInterrupt(0, wakeUp, LOW);//attaching a interrupt to pin d2
-//  set_sleep_mode(SLEEP_MODE_PWR_DOWN);//Setting the sleep mode, in our case full sleep
-//  digitalWrite(LED_BUILTIN, LOW); //turning LED off
-//  delay(1000); //wait a second to allow the led to be turned off before going to sleep
-//  sleep_cpu();//activating sleep mode
-//  Serial.println("just woke up!");//next line of code executed after the interrupt
-//  digitalWrite(LED_BUILTIN, HIGH); //turning LED on
-//}
-
-//void wakeUp() {
-//  Serial.println("Interrrupt Fired");//Print message to serial monitor
-//  sleep_disable();//Disable sleep mode
-//  detachInterrupt(0); //Removes the interrupt from pin 2;
-//}
