6f22d1c1b5e3758517ad9759f456bdc4118c61ad
Guy Dvir <guydvir@Guy-MacBookPro.local>
2020-07-04 18:47:25 +0300

    minors

--- sketch_jun25a/sketch_jun25a.ino
+++ sketch_jun25a/sketch_jun25a.ino
@@ -1,24 +1,37 @@
-// int Pins[]={34,35,32,33};
-int Pins[]={A5,A4,A3,A2};
-void setup() {
+int Pins[] = {34, 35, 32, 33};
+const char *dirs[] = {"UP", "LEFT", "DOWN", "RIGHT"};
+// int Pins[]={5,4,3,2};
+void setup()
+{
   // put your setup code here, to run once:
 
   Serial.begin(9600);
-  for(int a=0; a<4; a++){
+  for (int a = 0; a < 4; a++)
+  {
     pinMode(Pins[a], INPUT);
   }
-
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-   for(int a=0; a<4; a++){
+  for (int a = 0; a < 4; a++)
+  {
     // Serial.print("Sensor #");
     // Serial.print(a);
     // Serial.println(": ");
+    Serial.print(dirs[a]);
+    Serial.print(":");
     Serial.print(analogRead(Pins[a]));
     Serial.print(";");
   }
   Serial.println("");
+  Serial.print("Sides: ");
+      Serial.print(analogRead(Pins[0]) - analogRead(Pins[2]));
+  Serial.print("; ");
+  Serial.print("Pitch: ");
+      Serial.print(analogRead(Pins[1]) - analogRead(Pins[3]));
+  Serial.println("");
+
   delay(1000);
 }
